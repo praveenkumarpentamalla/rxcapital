6cc4e774691e128fb45815778367249d4ef42fdf
Praveen Kumar Pentamalla <103409229+praveenkumar0222@users.noreply.github.com>
2025-07-12 14:53:52 +0530

    Update settings.py

--- rxcapital/settings.py
+++ rxcapital/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = 'r!gdf6q%kkk!q$ea-5axt54oj&zae5myhsq$zfn&r355(16=l1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.95.17.193', 'localhost', '127.0.0.1']
 import os
 
 # Application definition
